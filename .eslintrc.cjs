badbba1247071465c2efcab0c52888b250bef7c0
kirill petrov <mail@kirill-petrov.ru>
2022-11-07 02:01:40 +0300

    installing and configuring dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  extends: [
+    "plugin:vue/vue3-strongly-recommended",
+    "eslint:recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+};
