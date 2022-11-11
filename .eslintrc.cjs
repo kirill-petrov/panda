e158b3f1fd65b0411db3030780282777d00dd803
kirill petrov <mail@kirill-petrov.ru>
2022-11-11 23:18:25 +0300

    добавлен сайдбар и строка поиска

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,49 @@ module.exports = {
   },
 
   extends: [
-    "plugin:vue/vue3-strongly-recommended",
-    "eslint:recommended",
-    "prettier",
+    'plugin:vue/vue3-strongly-recommended',
+    'eslint:recommended',
+    'prettier',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["vue"],
-  rules: {},
+  plugins: ['vue'],
+  rules: {
+    'no-console': process.env.NODE_ENV !== 'production' ? 'off' : 'error',
+    'no-debugger': process.env.NODE_ENV !== 'production' ? 'off' : 'error',
+    'no-unused-vars': process.env.NODE_ENV !== 'production' ? 'warn' : 'error',
+    'no-unused-expressions': 'warn',
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: [],
+      },
+    ],
+    'vue/attributes-order': [
+      'warn',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          ['UNIQUE', 'SLOT'],
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+      },
+    ],
+  },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <!-- Navigation -->
-  <nav>
-    <ul class="my-3 flex justify-center gap-4">
-      <li>
-        <router-link to="/" class="hover:border-b-2 hover:border-indigo-600"
-          >Home</router-link
-        >
-      </li>
-      <li>
-        <router-link
-          to="/tailwind"
-          class="hover:border-b-2 hover:border-indigo-600"
-          >tailwind-preview</router-link
-        >
-      </li>
-    </ul>
-  </nav>
-
   <!-- Content -->
-  <div class="px-4">
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
-nav a.router-link-active {
-  color: tomato;
-}
+// nav a.router-link-active {
+//   color: tomato;
+// }
 
 .helper {
   margin: 1rem;
